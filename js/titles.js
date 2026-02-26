2526a92aeb4ee8a0038c81752a75fac4fafa519e
asdasdasda sdasd <165959957+PicklefartmucnhingeaterHIi1234@users.noreply.github.com>
2026-02-25 23:57:04 -0500

    Update titles.js

--- js/titles.js
+++ js/titles.js
@@ -1,3 +1,4 @@
-const rrr=["ihy",":(","why?"];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
+const rrr=["superstar","beel","^_^"];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
 setInterval(yyy,500)
+
 
